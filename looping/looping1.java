160e2903ed02f96fabdea2099c36e464c52eefb5
sam-shervin <samshervin.sebastian@gmail.com>
2024-07-22 11:05:50 +0530

    looping

---
+++ looping/looping1.java
@@ -0,0 +1,10 @@
+package looping;
+
+public class looping1 {
+    public static void main(String[] args) {
+        String[] names = {"Harry", "Hermoine", "Ron Weasley"};
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
--- switch_case/switch_case.java
+++ switch_case/switch_case.java
@@ -1,7 +1,9 @@
-import java.util.*;
+package switch_case;
+import java.util.Scanner;
 
 public class switch_case{
     public static void main(String[] args) {
+        
         //get input and store in expression
         System.out.println("Enter an expression: ");
         Scanner sc = new Scanner(System.in);
