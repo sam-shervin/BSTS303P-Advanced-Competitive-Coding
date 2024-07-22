6b49ea815132956ae4d9ec715aafb30a6935dc4d
sam-shervin <samshervin.sebastian@gmail.com>
2024-07-22 11:02:02 +0530

    switch_case

---
+++ switch-case/switch_case.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class switch_case{
+    public static void main(String[] args) {
+        //get input and store in expression
+        System.out.println("Enter an expression: ");
+        Scanner sc = new Scanner(System.in);
+        int expression = sc.nextInt();
+        sc.close();
+        
+        //switch case
+
+        switch (expression) {
+            case 1:
+                System.out.println("Case 1");
+                break;
+            case 2:
+                System.out.println("Case 2");
+                break;
+            default:
+                System.out.println("Default case");
+        }
+    }
+}
